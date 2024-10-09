e6bf11681277aafb0af62099505740e05f021140
Sanketydv1 <sanketkumar1919@gmail.com>
2024-10-10 00:36:25 +0530

    3rd commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.ttk import Combobox
+from tkinter import messagebox
 import time
 
 win = Tk()
@@ -27,11 +28,23 @@ def Main_screen():
         frm.destroy()
         newuser_screen()
     def login():
+        acn=e_acn.get()
+        pwd=e_pass.get()
+        if len(acn)==0 or len(pwd)==0:
+            messagebox.showwarning('validation','Empty field is not allowed!')
+            return
+        elif len(acn)>=4 or len(pwd)>=4:
+            messagebox.showwarning('validation','length of Id & pass is > 3')
+            return
+        
         frm.destroy()
         welcome_screen()
+    def clear():
+        e_acn.delete(0,'end')
+        e_pass.delete(0,'end')
+        e_acn.focus()
     
     
-
     lbl_acn=Label(frm,text='ACN',font=('arial',20,'bold'),bg='powder blue')
     lbl_acn.place(relx=.3,rely=.1)
     
@@ -48,7 +61,7 @@ def Main_screen():
     btn_login = Button(frm,text='Login',font=('arial',16,'bold'),bd=4,command=login)
     btn_login.place(relx=.49,rely=.3)
     
-    btn_clear = Button(frm,text='Clear',font=('arial',16,'bold'),bd=4)
+    btn_clear = Button(frm,text='Clear',font=('arial',16,'bold'),bd=4,command=clear)
     btn_clear.place(relx=.6,rely=.3)
     
     btn_fpass = Button(frm,text='Forgot Password',command=forgotpass,font=('arial',16,'bold'),bd=4)
@@ -66,6 +79,12 @@ def forgotpass_screen():
         frm.destroy()
         Main_screen()
         
+    def clear():
+        e_acn.delete(0,'end')
+        e_mail.delete(0,'end')
+        e_mob.delete(0,'end')
+        e_acn.focus()
+        
     btn_new=Button(frm,text='Back',font=('arial',16,'bold'),bd=4,command=back)
     btn_new.place(relx=0,rely=0)
     
@@ -80,19 +99,19 @@ def forgotpass_screen():
     lbl_mail=Label(frm,text='Email',font=('arial',20,'bold'),bg='powder blue')
     lbl_mail.place(relx=.3,rely=.2)
     
-    e_mail =Entry(frm,font=('arial',20,'bold'),bd=4,show='*')
+    e_mail =Entry(frm,font=('arial',20,'bold'),bd=4)
     e_mail.place(relx=.45 , rely=.2)
     
     lbl_mob=Label(frm,text='Mobile.no',font=('arial',20,'bold'),bg='powder blue')
     lbl_mob.place(relx=.3,rely=.3)
     
-    e_mob =Entry(frm,font=('arial',20,'bold'),bd=4,show='*')
+    e_mob =Entry(frm,font=('arial',20,'bold'),bd=4)
     e_mob.place(relx=.45 , rely=.3)
     
     btn_sub=Button(frm,text='Submit',font=('arial',16,'bold'),bd=4)
     btn_sub.place(relx=.45,rely=.5)
     
-    btn_clr=Button(frm,text='Clear',font=('arial',16,'bold'),bd=4)
+    btn_clr=Button(frm,text='Clear',font=('arial',16,'bold'),bd=4,command=clear)
     btn_clr.place(relx=.6,rely=.5)
     
 def newuser_screen():
@@ -103,6 +122,13 @@ def newuser_screen():
     def back():
         frm.destroy()
         Main_screen()
+    def clear():
+        e_name.delete(0,'end')
+        e_pass.delete(0,'end')
+        e_mail.delete(0,'end')
+        e_mob.delete(0,'end')
+        cd_gender.delete(0,'end')
+        cd_acntype.delete(0,'end')
         
     btn_new=Button(frm,text='Back',font=('arial',16,'bold'),bd=4,command=back)
     btn_new.place(relx=0,rely=0)
@@ -124,13 +150,13 @@ def newuser_screen():
     lbl_mail=Label(frm,text='Email',font=('arial',20,'bold'),bg='powder blue')
     lbl_mail.place(relx=.3,rely=.3)
     
-    e_mail =Entry(frm,font=('arial',20,'bold'),bd=4,show='*')
+    e_mail =Entry(frm,font=('arial',20,'bold'),bd=4)
     e_mail.place(relx=.45 , rely=.3)
     
     lbl_mob=Label(frm,text='Mobile.no',font=('arial',20,'bold'),bg='powder blue')
     lbl_mob.place(relx=.3,rely=.4)
     
-    e_mob =Entry(frm,font=('arial',20,'bold'),bd=4,show='*')
+    e_mob =Entry(frm,font=('arial',20,'bold'),bd=4)
     e_mob.place(relx=.45 , rely=.4)
     
     lbl_gender=Label(frm,text='Gender',font=('arial',20,'bold'),bg='powder blue')
@@ -148,7 +174,7 @@ def newuser_screen():
     btn_sub=Button(frm,text='Submit',font=('arial',16,'bold'),bd=4)
     btn_sub.place(relx=.45,rely=.7)
     
-    btn_clr=Button(frm,text='Clear',font=('arial',16,'bold'),bd=4)
+    btn_clr=Button(frm,text='Clear',font=('arial',16,'bold'),bd=4,command=clear)
     btn_clr.place(relx=.6,rely=.7)
     
 def welcome_screen():
@@ -158,13 +184,82 @@ def welcome_screen():
     
     def logout():
         frm.destroy()
-        Main_screen()
+        Main_screen() 
         
+    def details():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_udtls=Label(ifrm,text='User details ',font=('arial',20,'bold'),bg='white')
+       lbl_udtls.pack()
+       
+    def update():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_upd=Label(ifrm,text='User updations ',font=('arial',20,'bold'),bg='white')
+       lbl_upd.pack()
+       
+    def deposite():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_dpt=Label(ifrm,text='User deposites ',font=('arial',20,'bold'),bg='white')
+       lbl_dpt.pack()
+        
+    def widthdrawl():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_wdt=Label(ifrm,text='User widthdrawls ',font=('arial',20,'bold'),bg='white')
+       lbl_wdt.pack()
+       
+    def transfer():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_trfs=Label(ifrm,text='User transfers ',font=('arial',20,'bold'),bg='white')
+       lbl_trfs.pack()
+        
+    def netbanking():
+       ifrm=Frame(frm,highlightbackground='gray',highlightthickness=2)
+       ifrm.configure(bg='white')
+       ifrm.place(relx=.2,rely=.1,relwidth=.72,relheight=.58)
+       
+       lbl_nbnk=Label(ifrm,text='Users online banking ',font=('arial',20,'bold'),bg='white')
+       lbl_nbnk.pack()
+    
     lbl_wel=Label(frm,text='Welcome,',font=('arial',20,'bold'),bg='powder blue')
     lbl_wel.place(relx=0,rely=0)
         
-    btn_log=Button(frm,text='Logout',font=('arial',16,'bold'),bd=4,command=logout)
+    btn_log=Button(frm,text='Logout',font=('arial',16,'bold'),bd=4, command=logout)
     btn_log.place(relx=.92,rely=0)
+    
+    btn_details=Button(frm,command=details, width=16,text='Details',font=('arial',16,'bold'),bd=4)
+    btn_details.place(relx=.0,rely=.1)
+    
+    btn_update=Button(frm,command=update,width=16,text='Update',font=('arial',16,'bold'),bd=4)
+    btn_update.place(relx=.0,rely=.2)
+    
+    btn_deposite=Button(frm, command=deposite,width=16,text='Deposite',font=('arial',16,'bold'),bd=4)
+    btn_deposite.place(relx=.0,rely=.3)
+    
+    btn_wdrl=Button(frm,command=widthdrawl,width=16,text='Widthdrawal',font=('arial',16,'bold'),bd=4)
+    btn_wdrl.place(relx=.0,rely=.4)
+    
+    btn_transfer=Button(frm ,command=transfer ,width=16,text='Transfer',font=('arial',16,'bold'),bd=4)
+    btn_transfer.place(relx=.0,rely=.5)
+    
+    btn_nbnk=Button(frm,command=netbanking,width=16,text='Net-banking',font=('arial',16,'bold'),bd=4)
+    btn_nbnk.place(relx=.0,rely=.6)
+    
+    
+   
         
 
 
